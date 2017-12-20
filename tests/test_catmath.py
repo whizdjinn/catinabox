c4226f988360d3a4eb0a1119c9b7d1f633afda76
Whizdjinn <marora.whizz@gmail.com>
2017-12-20 14:49:44 +0530

    Add tests to test_catmath.py
    Added tests to test_catmath.py

--- tests/test_catmath.py
+++ tests/test_catmath.py
@@ -2,15 +2,18 @@ from catinabox import catmath
 
 
 def test__cat_years_to_hooman_years__middle_age__succeeds():
-    assert True
+    hooman_years = catmath.cat_years_to_hooman_years(10)
+    assert hooman_years == 50
 
 
 def test__cat_years_to_hooman_years__less_than_one_year__succeeds():
-    assert True
+    hooman_years = catmath.cat_years_to_hooman_years(0.5)
+    assert hooman_years == 2.5
 
 
 def test__cat_years_to_hooman_years__0__returns_0():
-    assert True
+    hooman_years = catmath.cat_years_to_hooman_years(0)
+    assert hooman_years == 0
 
 
 # BONUS MATERIAL FOR STEP 2
